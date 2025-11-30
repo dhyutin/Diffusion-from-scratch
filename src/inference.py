804482733ed6b5e71a02dea1d916a0b624ac6d12
dhyutin <sreedhyutin@gmail.com>
2025-11-30 08:30:08 -0600

    inf update

--- src/inference.py
+++ src/inference.py
@@ -1,9 +1,11 @@
-
-
 import torch
+import imageio
+import numpy as np
+from torchvision.utils import make_grid
 
 
 device = "cuda"
+
 
 @torch.no_grad()
 def p_sample(model, x_t, t, T):
@@ -14,63 +16,34 @@ def p_sample(model, x_t, t, T):
     alpha_t = alphas[t]
     alpha_bar_t = alpha_bar[t]
 
-    # predict noise at this time step
     eps_theta = model(x_t, torch.tensor([t], device=x_t.device).repeat(x_t.size(0)))
 
-    # compute the DDPM mean
     coef1 = 1 / torch.sqrt(alpha_t)
     coef2 = beta_t / torch.sqrt(1 - alpha_bar_t)
 
     mean = coef1 * (x_t - coef2 * eps_theta)
-
-    # final step
     if t == 0:
         return mean
     noise = torch.randn_like(x_t)
     return mean + torch.sqrt(beta_t) * noise
 
 
-
 @torch.no_grad()
 def sample_images_progress(model, T, num_images=1, img_size=64, save_every=10):
     x = torch.randn(num_images, 3, img_size, img_size).to(device)
 
-    all_samples = {}  # to store step → image
+    all_samples = {} 
 
-    for step in reversed(range(T)):   # T → 0
+    for step in reversed(range(T)):
         x = p_sample(model, x, step, T)
 
-        if step % save_every == 0 or step == 0:   # save at intervals
+        if step % save_every == 0 or step == 0:
             # convert from [-1,1] to [0,1]
             x_vis = (x.clamp(-1, 1) + 1) / 2
             all_samples[step] = x_vis.clone().cpu()
 
     return all_samples
 
-
-def show_progress(samples_dict, T):
-    for step, imgs in sorted(samples_dict.items(), reverse=True):
-        grid = vutils.make_grid(imgs, nrow=2)
-        plt.figure(figsize=(6,6))
-        plt.title(f"Step {step}")
-        plt.imshow(grid.permute(1,2,0).numpy())
-        plt.axis("off")
-        plt.show()
-
-
-
-
-import imageio
-import numpy as np
-
-import imageio
-import numpy as np
-from PIL import Image, ImageDraw, ImageFont
-import torchvision.utils as vutils
-
-import imageio
-import numpy as np
-from torchvision.utils import make_grid
 
 def save_video(samples_dict, T, filename="ddpm_samples.mp4", fps=30):
     frames = []
